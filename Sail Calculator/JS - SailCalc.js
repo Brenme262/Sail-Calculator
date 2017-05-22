ba4b69d5f2f3ed6c2cb22ffc06602bfffc9b9122
Brenme262 <ofdoom@comcast.net>
2017-05-22 14:32:58 -0700

    Added Comfort Ratio to Form

--- Sail Calculator/JS - SailCalc.js
+++ Sail Calculator/JS - SailCalc.js
@@ -1,12 +1,16 @@
-
+///raw data var
 var sailArea = 0
 var displacement = 0
 var beam = 0
 var waterlineLength = 0
+var lengthOverAll = 0
+
+///calculated var
 var displacementCubicFeet = 0
 var sailAreaRatio = 0
 var capsizeScore = 0
 var displacementLengthRatio = 0 
+var comfortScore = 0
 
 
 function worker(){
@@ -14,6 +18,8 @@ function worker(){
 	displacement= document.getElementById("Displacement").value;
 	beam=document.getElementById("Beam").value;
 	waterlineLength=document.getElementById("LWL").value;
+	lengthOverAll=document.getElementById("LOA").value;
+	
 	
 	displacementCubicFeet = displacement / 64;
 	
@@ -28,6 +34,10 @@ function worker(){
 	
 	waterlineLengthRatio();
 	///console.log(waterlineLength)
+	
+	brewerComfortScore();
+	///console.log(comfortScore);
+	///console.log(lengthOverAll)
 	
 	printResults();
 };
@@ -50,18 +60,28 @@ function waterlineLengthRatio(){
 	
 };
 
+function brewerComfortScore(){
+	comfortScore = displacement / (.65 *((waterlineLength * .7) +(lengthOverAll *.3)) * Math.pow(beam, 1.33));
+	
+};
 
 function printResults(){
+	
+		
+	let sailAreaRatioResult = `<tr><td> Sail Area / Displacment Ratio: </td> <td> ${sailAreaRatio}</td></tr>`;
+	let capsizeScreeningResult = `<tr> <td>Capsize Screening Score:</td> <td>${capsizeScore}</td> </tr>`;
+	let displacementLengthResult = `<tr> <td>Displacement-Length Ratio:</td> <td>${displacementLengthRatio}</td> </tr>`;
+	let comfortScoreResult = `<tr> <td>Brewer Comfort Score: </td> <td>${comfortScore}</td> </tr>`;
+	
 	///Results as HTML Table Elements
 	let tableHead = `<table><tr><th>Name</th><th>Value</th></tr><tr>`;
 	let tableFoot = `</table>`;
-	let sailAreaRatioResult = `<tr><td> Sail Area / Displacment Ratio: </td> <td> ${sailAreaRatio}</td></tr>`;
-	let capsizeScreeningResult = `<tr> <td>Capsize Screening Score:</td> <td>${capsizeScore}</td> </tr>`;
-	let displacementLengthResult = `<tr> <td>Displacement-Length Ratio:</td> <td>${displacementLengthRatio}</td> </tr>`
+	let tableBody = `${sailAreaRatioResult} ${capsizeScreeningResult} ${displacementLengthResult} ${comfortScoreResult}`;
+
 	
 	
 	///Results combined into table
-	let results = `${tableHead} ${sailAreaRatioResult} ${capsizeScreeningResult} ${displacementLengthResult} ${tableFoot}`;
+	let results = `${tableHead} ${tableBody}  ${tableFoot}`;
 	
 	document.getElementById("results").innerHTML = results;
 
